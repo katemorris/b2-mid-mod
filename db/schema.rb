3d8ff99000c2a60f3e34b9c7b691478c5504197a
Kate Tester <katemorris@gmail.com>
2020-10-23 09:39:25 -0600

    Adds rides and relationship to parks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_23_152255) do
+ActiveRecord::Schema.define(version: 2020_10_23_153715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,12 @@ ActiveRecord::Schema.define(version: 2020_10_23_152255) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "rides", force: :cascade do |t|
+    t.string "name"
+    t.integer "thrill_rating"
+    t.bigint "park_id"
+    t.index ["park_id"], name: "index_rides_on_park_id"
+  end
+
+  add_foreign_key "rides", "parks"
 end
