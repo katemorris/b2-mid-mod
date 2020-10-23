f4d970f8a67894310332eb79d5e4830a0274d42d
Kate Tester <katemorris@gmail.com>
2020-10-23 11:24:05 -0600

    Adds path helpers

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/mechanics', to: 'mechanics#index'
-  get '/mechanics/:id', to: 'mechanics#show'
+  get '/mechanics/:id', to: 'mechanics#show', as: 'mechanic'
   patch '/mechanics/:id', to: 'mechanics#update'
 
-  get '/parks/:id', to: 'parks#show'
+  get '/parks/:id', to: 'parks#show', as: 'park'
 end
