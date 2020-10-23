bb88345a894e09d5ace7935b1a0b166034594bbe
Kate Tester <katemorris@gmail.com>
2020-10-23 09:30:52 -0600

    Adds test for relationship to rides

--- spec/models/park_spec.rb
+++ spec/models/park_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe Parl, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Park, type: :model do
+  describe 'relationships' do
+    it { should have_many :rides }
+  end
 end
