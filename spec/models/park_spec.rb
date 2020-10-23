eee5ce69f594c3807336bbfa586d68e4757f330a
Kate Tester <katemorris@gmail.com>
2020-10-23 10:38:12 -0600

    Adds test for upcoming avg thrill method.

--- spec/models/park_spec.rb
+++ spec/models/park_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe Park, type: :model do
   describe 'relationships' do
     it { should have_many :rides }
   end
+
+  describe 'instance methods' do
+    it '.avg_thrill' do
+      six_flags = Park.create(name: 'Six Flags', price: 35.50)
+      nitro = Ride.create(name: 'Nitro', thrill_rating: 7)
+      skull_mountain = Ride.create(name: 'Skull Mountain', thrill_rating: 4)
+
+      expect(six_flags.avg_thrill.round(1)).to eq(5.5)
+    end
+  end
 end
