ae0a2119216f8d0435117f8e9980621e84de8315
Kate Tester <katemorris@gmail.com>
2020-10-23 10:54:21 -0600

    Adds two tests for show page info and adding ride form.

---
+++ spec/features/mechanics/show_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+describe "When I visit a mechanic's show page" do
+  before (:each) do
+    @sue = Mechanic.create(name: 'Sue Markell', years_exp: 10)
+    @six_flags = Park.create(name: 'Six Flags', price: 35.50)
+    @nitro = Ride.create(
+      name: 'Nitro',
+      thrill_rating: 8,
+      park_id: @six_flags.id
+    )
+    @skull_mountain = Ride.create(
+      name: 'Skull Mountain',
+      thrill_rating: 4,
+      park_id: @six_flags.id
+    )
+    @toro = Ride.create(
+      name: 'El Toro',
+      thrill_rating: 8,
+      park_id: @six_flags.id
+    )
+    sue_nitro = RideMechanic.create(mechanic: @sue, ride: @nitro)
+    sue_toro = RideMechanic.create(mechanic: @sue, ride: @toro)
+  end
+
+  it 'I see their data and a way to add a ride to their workload' do
+    visit "/mechanics/#{@sue.id}"
+
+    expect(page).to have_content(@sue.name)
+    expect(page).to have_content("Years of Experience: #{@sue.years_exp}")
+
+    within('.rides') do
+      expect(page).to have_content("Current Rides Assigned:")
+      expect(page).to have_content(@nitro.name)
+      expect(page).to have_content(@toro.name)
+    end
+
+    expect(page).to have_content("Add a ride to workload")
+    expect(page).to have_field("Ride ID")
+  end
+
+  it 'I can add a ride to their workload' do
+    visit "/mechanics/#{@sue.id}"
+
+    fill_in :ride_id, with: @skull_mountain.id
+    click_on 'Submit'
+    expect(current_path).to eq("/mechanics/#{@sue.id}")
+
+    within('.rides') do
+      expect(page).to have_content("Current Rides Assigned:")
+      expect(page).to have_content(@nitro.name)
+      expect(page).to have_content(@toro.name)
+      expect(page).to have_content(@skull_mountain.name)
+    end
+  end
+
+end
